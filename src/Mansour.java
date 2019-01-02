fd7578f2cf3c93b0ab9b4f1d9ae1feb9237c0688
mansour <mansourx201@gmail.com>
2019-01-03 02:10:36 +0300

    first commit

---
+++ src/Mansour.java
@@ -0,0 +1,19 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+public class Mansour extends JFrame{
+	
+	
+	JFrame f;
+	 Mansour() {
+		JButton button = new JButton("Mansour");
+		button.setBounds(120, 100, 150, 60);
+		add(button);
+		setSize(600, 500);
+		setLayout(null);
+		setVisible(true);
+	}
+
+	
+}
---
+++ src/Sorwrornot.java
@@ -0,0 +1,101 @@
+import java.util.Scanner; 
+
+public class Sorwrornot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner (System.in);
+	
+		
+	System.out.println("Please Tell the Charctar");
+		String x = in.nextLine();
+
+		if (x.equals("Luffy")) {
+			
+			System.out.println("Captain , not Sworder");
+		}
+		else if (x.equals("Zoro")) {
+			
+			System.out.println("Swordsman ");
+		}
+		else if (x.equals("Sanji")) {
+			
+			System.out.println("Cook");
+		}
+		else if (x.equals("Robin")) {
+			
+			System.out.println("Archaeologist");
+		}	
+		else if (x.equals("Chopper")) {
+			
+			System.out.println("Doctor , not Sworder");
+		}
+		else if (x.equals("Nami")) {
+			
+			System.out.println("Navigator , not Sworder");
+		}
+		else if (x.equals("Usopp")) {
+			
+			System.out.println("Sniper , not Sworder");
+		}
+		else if (x.equals("Brook")) {
+			
+			System.out.println("Musician, Swordsman");
+		}
+		else if (x.equals("Franky")) {
+			
+			System.out.println("Shipwright , not Sworder");
+		}
+		else if (x.equals("Law")) {
+			
+			System.out.println("doctor ,  Swordman");
+		}
+
+		while (x.equals(x)) {
+			System.out.println("Please Tell the Charctar");
+			 x = in.nextLine();
+			
+	if (x.equals("Luffy")) {
+			
+			System.out.println("Captain , not Sworder");
+		}
+		else if (x.equals("Zoro")) {
+			
+			System.out.println("Swordsman");
+		}
+		else if (x.equals("Sanji")) {
+			
+			System.out.println("Cook");
+		}
+		else if (x.equals("Robin")) {
+			
+			System.out.println("Archaeologist");
+		}	
+		else if (x.equals("Chopper")) {
+			
+			System.out.println("Doctor , not Sworder");
+		}
+		else if (x.equals("Nami")) {
+			
+			System.out.println("Navigator , not Sworder");
+		}
+		else if (x.equals("Usopp")) {
+			
+			System.out.println("Sniper , not Sworder");
+		}
+		else if (x.equals("Brook")) {
+			
+			System.out.println("Musician, Swordsman");
+		}
+		else if (x.equals("Franky")) {
+			
+			System.out.println("Shipwright , not Sworder");
+		}
+		else if (x.equals("Law")) {
+			
+			System.out.println("doctor ,  Swordman ");
+		}
+		}
+	}
+
+}
